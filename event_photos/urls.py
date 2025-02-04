1523d98125e91a9a88c35339faa6c2bfaa884590
babbokem <massimiliano.cima@gmail.com>
2025-02-04 08:12:57 +0100

    modifica url

--- event_photos/urls.py
+++ event_photos/urls.py
@@ -45,6 +45,6 @@ urlpatterns = [
 
 
 # Configurazione per servire file statici e media solo in modalità DEBUG
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
